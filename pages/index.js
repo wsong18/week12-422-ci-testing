ecd0d702fb58195264909e22428d748fed2806eb
W Song <wei.song@senecacollege.ca>
2024-04-04 15:45:37 -0400

    Fix - added : after By before logo

--- pages/index.js
+++ pages/index.js
@@ -22,11 +22,11 @@ export default function Home() {
           </p>
           <div>
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://abc.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By:{' '}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
